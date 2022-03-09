d19f9ce52bec5ab07a0d22f1a69211c688c523c0
genghaoyan <92103217+genghaoyan@users.noreply.github.com>
2022-03-09 20:42:22 +0800

    Update Houseprice pridiction.py

--- House Prices/house-prices-advanced-regression-techniques/Houseprice pridiction.py
+++ House Prices/house-prices-advanced-regression-techniques/Houseprice pridiction.py
@@ -113,7 +113,7 @@ for col in none_lists:
 #说明文档里丢失的数据补为频率最高的值
 most_lists = ['MSZoning', 'Exterior1st', 'Exterior2nd', 'SaleType', 'KitchenQual', 'Electrical']
 for col in most_lists:
-    train[col] = train[col].fillna(train[col].mode()[0])
+    train[col] = train[col].fillna(train[col].mode()[0])  #mode()得到的结果是一个pd.Series，[0]是取其第一个元素，即出现次数最多的那个元素
     test[col] = test[col].fillna(train[col].mode()[0])    #注意这里补充的是训练集中出现最多的类别
 
 #Functional中补充Typ，Utilities无用处直接删去
@@ -130,9 +130,77 @@ for col in zero_lists:
     train[col] = train[col].fillna(0)
     test[col] = test[col].fillna(0)
 
-#补充为Neighborhood的中位数
-train['LotFrontage'] = train.groupby('Neighborhood')['LotFrontage'].apply(lambda x: x.fillna(x.median()))
+#补充为Neighborhood的平均数
+train['LotFrontage'] = train.groupby('LotConfig')['LotFrontage'].apply(lambda x: x.fillna(x.mean()))#以Neighborhood对LotFrontage进行聚合并在最后一行加上平均数
 for ind in test['LotFrontage'][test['LotFrontage'].isnull().values==True].index:
-    x = test['Neighborhood'].iloc[ind]
-    test['LotFrontage'].iloc[ind] = train.groupby('Neighborhood')['LotFrontage'].median()[x]
+    x = test['LotConfig'].iloc[ind]
+    test['LotFrontage'].iloc[ind] = train.groupby('LotConfig')['LotFrontage'].mean()[x]
+
+print(train.isnull().sum().any())#.sum统计空列表的个数
+
+#从存放类别特征的列表去掉'Utilities'
+cate_features.remove('Utilities')
+print('The number of categorical features:', len(cate_features))
+
+#对不能输入模型的类别特征进行编码，对于各个类别中可能存在顺序关系的，用LabelEncoder编码，对于不存在顺序关系的，用get_dummies进行编码
+for col in cate_features:
+    train[col] = train[col].astype(str)
+    test[col] = test[col].astype(str)
+le_features = ['Street', 'Alley', 'LotShape', 'LandContour', 'LandSlope', 'HouseStyle', 'RoofMatl', 'Exterior1st', 'Exterior2nd', 'ExterQual',
+               'ExterCond', 'Foundation', 'BsmtQual', 'BsmtCond', 'BsmtExposure', 'BsmtFinType1', 'BsmtFinType2', 'HeatingQC', 'CentralAir',
+               'KitchenQual', 'Functional', 'FireplaceQu', 'GarageFinish', 'GarageQual', 'GarageCond', 'PavedDrive', 'PoolQC', 'Fence']
+for col in le_features:
+    encoder = LabelEncoder()
+    value_train = set(train[col].unique())
+    value_test = set(test[col].unique())
+    value_list = list(value_train | value_test)
+    encoder.fit(value_list)
+    train[col] = encoder.transform(train[col])
+    test[col] = encoder.transform(test[col])
+
+#处理偏斜度大于0.5的数据
+skewness = train[num_features].apply(lambda x: skew(x)).sort_values(ascending=False)
+skewness = skewness[skewness>0.5]
+skew_features = skewness.index
+
+#增强模型对异常度的刚性，用Box Cox转换来处理偏斜数据
+for col in skew_features:
+    lam = stats.boxcox_normmax(train[col]+1)    #+1是为了保证输入大于零
+    train[col] = boxcox1p(train[col], lam)
+    test[col] = boxcox1p(test[col], lam)
+
+#依据经验构建的新的特征
+train['IsRemod'] = 1
+train['IsRemod'].loc[train['YearBuilt']==train['YearRemodAdd']] = 0  #是否翻新(翻新：1， 未翻新：0)
+train['BltRemodDiff'] = train['YearRemodAdd'] - train['YearBuilt']  #翻新与建造的时间差（年）
+train['BsmtUnfRatio'] = 0
+train['BsmtUnfRatio'].loc[train['TotalBsmtSF']!=0] = train['BsmtUnfSF'] / train['TotalBsmtSF']  #Basement未完成占总面积的比例
+train['TotalSF'] = train['TotalBsmtSF'] + train['1stFlrSF'] + train['2ndFlrSF']  #总面积
+#对测试集做同样的处理
+test['IsRemod'] = 1
+test['IsRemod'].loc[test['YearBuilt']==test['YearRemodAdd']] = 0  #是否翻新(翻新：1， 未翻新：0)
+test['BltRemodDiff'] = test['YearRemodAdd'] - test['YearBuilt']  #翻新与建造的时间差（年）
+test['BsmtUnfRatio'] = 0
+test['BsmtUnfRatio'].loc[test['TotalBsmtSF']!=0] = test['BsmtUnfSF'] / test['TotalBsmtSF']  #Basement未完成占总面积的比例
+test['TotalSF'] = test['TotalBsmtSF'] + test['1stFlrSF'] + test['2ndFlrSF']  #总面积
+
+#集中处理数据
+dummy_features = list(set(cate_features).difference(set(le_features)))
+all_data = pd.concat((train.drop('SalePrice', axis=1), test)).reset_index(drop=True)
+all_data = pd.get_dummies(all_data, drop_first=True)  #注意独热编码生成的时候要去掉一个维度，保证剩下的变量都是相互独立的
+
+#保存最终处理结果
+trainset = all_data[:1458]
+y = train['SalePrice']
+trainset['SalePrice'] = y.values
+testset = all_data[1458:]
+print('The shape of training data:', trainset.shape)
+print('The shape of testing data:', testset.shape)
+
+trainset.to_csv('train_data.csv', index=False)
+testset.to_csv('test_data.csv', index=False)
+
+
+
+
 
